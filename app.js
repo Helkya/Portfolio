c8e3c1283088f3dc8f157d58287e3cd0ef2e187c
Javi Palacios <javipalacios94@hotmail.com>
2021-05-06 21:25:50 +0200

    General structure in HTML. Sass files  and cube 3d animartion.

---
+++ app.js
@@ -0,0 +1,1 @@
+import * as get from './utils-js/selector';
---
+++ utils-js/selector.js
@@ -0,0 +1,14 @@
+const get = (selection) => {
+  const elements = [...document.querySelectorAll(selection)];
+  if (elements.length > 1) return elements;
+
+  if (selection.startsWith(1)) {
+    selection = selection.slice(1, selection.length);
+  }
+
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(`"${selection}" not found`);
+};
+
+export default getElement;
